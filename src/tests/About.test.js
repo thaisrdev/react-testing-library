1f28f41d7e5721f8daee96c545dc260f33fb6199
Thais Rosa <tsrosa18@gmail.com>
2022-09-25 02:03:33 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {}); //
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingPokedex = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+  it('Testa o link da imagem da Pokédex', () => {
+    renderWithRouter(<About />);
+    const imgPokedex = screen.getByRole('img');
+    const linkImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(imgPokedex.src).toBe(linkImg);
+  });
+});
