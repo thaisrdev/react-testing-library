f1984c9b32fb465b07dfb2451f4bd7ec7713f21a
Thais Rosa <tsrosa18@gmail.com>
2022-09-25 02:22:21 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it(
+    'Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+      const notFound = screen.getByRole('heading', {
+        name: 'Page requested not found',
+        level: 2,
+      });
+      expect(notFound).toBeInTheDocument();
+    },
+  );
+  it('Testa o link da imagem de not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImg = screen.getByRole('img');
+    const linkImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(notFoundImg.src).toBe(linkImg);
+  });
+});
